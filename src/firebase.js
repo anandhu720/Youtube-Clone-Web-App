65136f9292c276d0cdb92acf263b9bcf07eb1e4f
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2021-06-15 15:55:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCOimWFiN331cL3O0uvGqK1VEBpUKTa4fw",
-    authDomain: "yt-clone-7.firebaseapp.com",
+    apiKey: "****************",
+    authDomain: "*****************",
     projectId: "yt-clone-7",
-    storageBucket: "yt-clone-7.appspot.com",
-    messagingSenderId: "515268104112",
-    appId: "1:515268104112:web:cc59763df0f91b93ec9075",
-    measurementId: "G-3391PVHZKK"
+    storageBucket: "*************",
+    messagingSenderId:*********************,
+    appId: "*******************",
+    measurementId: "*********************"
   };
 
   firebase.initializeApp(firebaseConfig);
